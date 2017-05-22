10e0ffd40ca9edba8b2e76cf4087fd1b34737fe6
Gustavo Mitt <gustavo@actar.com.br>
2017-05-21 21:13:07 -0300

    Cronometro para Arduino pronto

--- cronometro.py
+++ cronometro.py
@@ -1,5 +1,7 @@
 import time
 import cronus.beat as beat
+import select
+import sys
 
 centesimo=0
 segundo=0
@@ -11,6 +13,9 @@ beat.set_rate(100)
 
 
 while beat.true():
+  char = select.select(sys.stdin, [], [], 0.1)[0]
+  if char:
+    print char
   print "%02d:%02d:%02d,%02d" % (hora,minuto,segundo,centesimo)
   centesimo=centesimo+1
   if centesimo>=100:
