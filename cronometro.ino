36691e8d0393887b04b15b3bee9710bdf34f9e55
Gustavo Mitt <gustavo@actar.com.br>
2017-05-27 16:58:38 -0300

    usando interrupcao para ter tempo regular

--- cronometro.ino
+++ cronometro.ino
@@ -1,41 +1,130 @@
+
+/*
+ Demo code of EB - I2C_1602_LCD 
+ by maker studio 
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+ 
+  The circuit:
+ * 5V to Arduino 5V pin
+ * GND to Arduino GND pin
+ * CLK to Analog #5
+ * DAT to Analog #4
+ 
+*/
+
+// include the library code:
 #include <Wire.h>
-#include <LCD.h>
-#include <LiquidCrystal_I2C.h>  // F Malpartida's NewLiquidCrystal library
+#include "LiquidCrystal.h"
+#include <TimerOne.h>
 
+// Connect via i2c, default address #0 (A0-A2 not jumpered)
+LiquidCrystal lcd(0);
 
-#define I2C_ADDR    0x20  // Define I2C Address for controller
-#define BACKLIGHT_PIN  15
-#define En_pin  6
-#define Rw_pin  5
-#define Rs_pin  4
-#define D4_pin  11
-#define D5_pin  12
-#define D6_pin  13
-#define D7_pin  14
+//int centesimo=0;
+volatile int decimo=0;
+volatile int segundo=0;
+volatile int minuto=0;
+volatile int hora=0; 
 
-#define  LED_OFF  0
-#define  LED_ON  1
-LiquidCrystal_I2C  lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
+volatile int unsigned long tempoEntreInterrupcoes;
 
-void setup() 
-{
-  lcd.begin (16,2);  // initialize the lcd
-// Switch on the backlight
-  lcd.setBacklightPin(BACKLIGHT_PIN,NEGATIVE);
-  lcd.setBacklight(LED_ON);
+// define pin button is attached to
+int pin=2;
+
+// define chrometer states
+int s_zeroed=0;
+int s_running=1;
+int s_stopped=2;
+
+int state = s_zeroed;
+
+void setup() {
+  // set up the LCD's number of rows and columns: 
+  lcd.begin(16, 2);
+
+  lcd.setBacklight(HIGH);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Cronometro");
+
+  pinMode(pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pin), pushButton, RISING);//once button been pushed, function pushButton() will be executed
+
+  Timer1.initialize(100000); // set a decisecond period
+  Timer1.attachInterrupt(incrementaTempo);
 }
 
-void loop()  
-{
+void loop() {
+  lcd.setCursor(0, 1);
+  if (hora<10)
+  {
+    lcd.print("0");
+  }
+  lcd.print(hora);
+  lcd.print(":");
+  if (minuto<10)
+  {
+    lcd.print("0");
+  }
+  lcd.print(minuto);
+  lcd.print(":");
+  if (segundo<10)
+  {
+    lcd.print("0");
+  }
+  lcd.print(segundo);
+  lcd.print(",");
+  lcd.print(decimo);
 
-// Reset the display 
-  lcd.clear();
-  delay(1000);
-  lcd.home();
- 
-// Print on the LCD
-  lcd.backlight();  
-  lcd.setCursor(0,0); 
-  lcd.print("Hello, world!");
-  delay(8000);
+  delay(50);
+}
+
+void pushButton()
+{
+  if ( (millis() - tempoEntreInterrupcoes) > 300)
+  {
+    tempoEntreInterrupcoes = millis();
+    
+    if(state==s_zeroed)
+    {
+      state = s_running;
+    } else {
+      if(state==s_running){
+        state=s_stopped;
+      } else {
+        if(state==s_stopped){
+          state=s_zeroed;
+          decimo=0;
+          segundo=0;
+          minuto=0;
+          hora=0; 
+        }
+      }
+    }
+  }
+}
+
+
+void incrementaTempo()
+{
+  if(state==s_running)
+  {
+    decimo=decimo+1;
+    if (decimo>=10)
+    {
+      segundo=segundo+1;
+      decimo=0;
+    }
+    if (segundo>=60)
+    {
+      minuto=minuto+1;
+      segundo=0;
+    }
+    if (minuto>=60)
+    {
+      hora=hora+1;
+      minuto=0;
+    }
+  }
 }
